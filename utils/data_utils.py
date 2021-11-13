71d8425747afe1b9fa62c5f5812b20bc86dd46b1
Bo Tang <botang@mie.utoronto.ca>
2021-11-13 18:20:00 -0500

    Clean code

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -14,17 +14,13 @@ def check_extension(filename):
 
 
 def save_dataset(dataset, filename):
-
     filedir = os.path.split(filename)[0]
-
     if not os.path.isdir(filedir):
         os.makedirs(filedir)
-
     with open(check_extension(filename), 'wb') as f:
         pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
 
 
 def load_dataset(filename):
-
     with open(check_extension(filename), 'rb') as f:
         return pickle.load(f)
