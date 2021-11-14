9ad5806c8b724faa1afe967fda7dc9682fc8906d
Bo Tang <botang@mie.utoronto.ca>
2021-11-14 12:37:34 -0500

    Add vis

---
+++ utils/vis.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Visualization
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
+def routesPlot(routes, depot, loc):
+    """
+    This is a mthod to draw plot of routes
+
+    Args:
+        routes: (list[list]): VRP solution
+        depot (array): coordinate of central depot
+        loc (array): coordinates of customers
+    """
+    # use different colors
+    cmap = cm.get_cmap("viridis")
+    colors = cmap(np.linspace(0, 1, len(routes)))
+    # figure size
+    plt.figure(figsize=(12, 8))
+    # coordinates of routes
+    for i, r in enumerate(routes):
+        x, y = [depot[0]], [depot[1]]
+        for v in r:
+            x.append(loc[v,0])
+            y.append(loc[v,1])
+        x.append(depot[0])
+        y.append(depot[1])
+        # plot route
+        plt.plot(x, y, c=colors[i], ls="-", lw=1, marker='.', ms=8, alpha=0.9)
+    # plot depot
+    plt.scatter(depot[0], depot[1], c='b', marker='p', s=150)
+    plt.show()
