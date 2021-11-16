9f2f60d43a29d6883a4484fda5ef1988bf52cba2
Bo Tang <botang@mie.utoronto.ca>
2021-11-15 21:25:41 -0500

    Add feasibility check

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@
 # coding: utf-8
 
 from utils.vis import routesPlot
+from utils.check import checkValid
---
+++ utils/check.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Author: Bo Tang
+"""
+Feasibility checking
+"""
+
+def checkValid(routes, depot, loc, demand):
+    """
+    A function to check solution feasibility
+    """
+    # init set
+    nodes = set(range(len(loc)))
+    # check
+    for r in routes:
+        left = 1
+        for v in r:
+            # unvisted nodes
+            if v not in nodes:
+                print("Node {} is visited or not existed.".format(v))
+                return False
+            # unsatisfied demand
+            left -= demand[v]
+            if left < -1e-7:
+                print("Demand of node {} cannot be satisfied.".format(v))
+                return False
+            # remove
+            nodes.remove(v)
+    # go through all node
+    if nodes:
+        print("Nodes", ", ".join(map(str, list(nodes))), "are not visited.")
+        return False
+    else:
+        return True
