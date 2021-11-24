94ed1fef21941c00fef8626f5bdec1440b6860b0
Bo Tang <botang@mie.utoronto.ca>
2021-11-23 22:14:09 -0500

    Update: cuda

--- train.py
+++ train.py
@@ -7,10 +7,12 @@ Deep Q-Learning
 
 import queue
 
+import torch
+
 from dgl.dataloading import GraphDataLoader
 from prob import VRPDGLDataset
 from attention_model import load_routing_agent
-
+from utils.state import routingState
 
 def train(model, size, step=10, lr=1e-4, batch=64, num_samples=1000, seed=135):
     """
@@ -25,33 +27,37 @@ def train(model, size, step=10, lr=1e-4, batch=64, num_samples=1000, seed=135):
         epoch(int): number of epochs
         num_samples(int): number of training instances
     """
+    # cuda
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    model.to(device)
     # load dataset
     print("Generating dataset...")
     data = VRPDGLDataset(num_samples=num_samples, seed=seed)
     dataloader = GraphDataLoader(data, batch_size=batch)
-
     # init memory
     mem = memInit()
-
     # set optimizer
     # optimizer = optim.Adam(model.parameters(), lr=lr)
-
     # set time horizon
     horizon = 2 * size
-
     # load routing agent
+    print("\nLoad routing agent...")
     rou_agent = load_routing_agent(size=size)
-
-    print("Training model...")
+    print("\nTraining model...")
     for batch_data, batch_graph in dataloader:
+        # to device
+        batch_graph.to(device)
+        batch_data["loc"] = batch_data["loc"].to(device)
+        batch_data["demand"] = batch_data["demand"].to(device)
+        batch_data["depot"] = batch_data["depot"].to(device)
         # set state-action queue
         sa_queue = queue.Queue()
-
         # initialize routing state
         rou_state = rou_agent.re_init(batch_data['loc'])
-
         # init state
-        state = stateInit()
+        state = routingState(batch_data)
         for t in range(horizon):
             # take action
             action = actionDecode(batch_graph, state)
@@ -67,6 +73,7 @@ def train(model, size, step=10, lr=1e-4, batch=64, num_samples=1000, seed=135):
                 mem = memUpdate(mem)
                 # update model parameters
                 modelUpdate(model, mem)
+            break
 
 
 def memInit():
