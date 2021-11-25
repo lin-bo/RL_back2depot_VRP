807b98ca7bf824f323319d878854184bda029a99
Bo Tang <botang@mie.utoronto.ca>
2021-11-25 18:57:30 -0500

    Bug fix: bool var

--- utils/r2d_agent.py
+++ utils/r2d_agent.py
@@ -15,7 +15,7 @@ class returnAgent:
     This class is return-to-depot agent
     """
 
-    def __init__(self, gnn_x_feat, gnn_w_feats, gnn_e_feats):
+    def __init__(self, gnn_x_feat, gnn_w_feats, gnn_e_feats, gamma):
         # nn
         self.q_gnn = QGNN(x_feats=gnn_x_feat, w_feats=gnn_w_feats, e_feats=gnn_e_feats)
         # cuda
@@ -25,8 +25,22 @@ class returnAgent:
         self.q_gnn = self.q_gnn.to(self.device)
         # optimizer
         self.optim = optim.Adam(self.q_gnn.parameters(), lr=0.0001)
+        # recay rate
+        self.gamma = gamma
 
     def actionDecode(self, batch_graph, state):
+        """
+        A method to decode action
+
+        Args:
+          batch_graph (DGL graph): a batch of graphs
+          state (returnState): enviroment state
+        """
+        self.q_gnn.eval()
+        action, _ = self.getMaxQ(batch_graph, state)
+        return action
+
+    def getMaxQ(self, batch_graph, state):
         """
         A method to decode action
 
@@ -39,7 +53,6 @@ class returnAgent:
         action_return = - torch.ones((batch,1), device=self.device)
         action_noreturn = torch.ones((batch,1), device=self.device)
         # calculate Q value
-        self.q_gnn.eval()
         q_r = self.q_gnn(batch_graph, state, action_return)
         q_n = self.q_gnn(batch_graph, state, action_noreturn)
         q = torch.cat((q_r, q_n), 1)
@@ -47,7 +60,7 @@ class returnAgent:
         qind = torch.argmax(q, dim=1).reshape(batch, 1)
         # force to not return on depot
         for i in range(batch):
-            if state.v[i].item() == 0 and torch.all(state.o[i,1:]).item():
+            if state.v[i].item() == 0 and not torch.all(state.o[i,1:]).item():
                 qind[i,0] = 1
         action = ((qind - 0.5) * 2).to(torch.int32)
         qvalue = q.gather(dim=1, index=qind)
@@ -60,4 +73,5 @@ class returnAgent:
         Args:
           record (namedtuple): a record of MDP steps
         """
-        pass
+        s_p, a_p, r_pt, s_t = record.s_p, record.a_p, record.r_pt, record.s_t
+        print(a_p)
