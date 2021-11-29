06704c007001d4b7169f456be008ed81c15ce355
Bo Tang <botang@mie.utoronto.ca>
2021-11-29 18:47:35 -0500

    Bug fix: output

--- solver/am_vrp_solver.py
+++ solver/am_vrp_solver.py
@@ -13,7 +13,6 @@ class amVRP:
         args:
             size: the number of customers
         """
-
         self.device = "cpu"
         if torch.cuda.is_available():
             self.device = "cuda"
@@ -21,16 +20,31 @@ class amVRP:
         self.solver = load_routing_agent(size=size, name='vrp')
         self.horizon = size * 2
         self._size = size
+        assert method in ['greedy', 'sampling']
         self.method = method
-        assert method not in ['greedy', 'sampling']
 
     def solve(self, batch_data):
-
         if self.method == 'greedy':
             batch_rep, iter_rep = 1, 1
         else:
             batch_rep, iter_rep = 50, 1
-
         routes, costs = self.solver.sample_many(batch_data, batch_rep=batch_rep, iter_rep=iter_rep)
+        routes = self._covertRoutes(routes)
+        return routes, costs.detach().cpu().tolist()
 
-        return routes, costs
+    def _covertRoutes(self, batch_routes):
+        batch_routes = batch_routes.cpu().detach().numpy() - 1
+        batch_routes_list = []
+        for routes in batch_routes:
+            routes_list = []
+            tour_list = []
+            for i in routes:
+                if i == -1 and len(tour_list) != 0:
+                    routes_list.append(tour_list)
+                    tour_list = []
+                if i != -1:
+                    tour_list.append(i)
+            if len(tour_list) != 0:
+                routes_list.append(tour_list)
+            batch_routes_list.append(routes_list)
+        return batch_routes_list
