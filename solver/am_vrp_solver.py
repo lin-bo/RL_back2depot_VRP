6d84f343e5f829051b20677cb179dbeb82860566
Bo Lin <im.linbo.96@gmail.com>
2021-11-29 18:01:07 -0500

    add sampling decode

--- solver/am_vrp_solver.py
+++ solver/am_vrp_solver.py
@@ -8,7 +8,7 @@ from solver.absolver import ABSolver
 
 class amVRP:
 
-    def __init__(self, size=20):
+    def __init__(self, size=20, method='sampling'):
         """
         args:
             size: the number of customers
@@ -21,9 +21,16 @@ class amVRP:
         self.solver = load_routing_agent(size=size, name='vrp')
         self.horizon = size * 2
         self._size = size
+        self.method = method
+        assert method not in ['greedy', 'sampling']
 
     def solve(self, batch_data):
 
-        routes, costs = self.solver.sample_many(batch_data, batch_rep=1, iter_rep=1)
+        if self.method == 'greedy':
+            batch_rep, iter_rep = 1, 1
+        else:
+            batch_rep, iter_rep = 50, 1
+
+        routes, costs = self.solver.sample_many(batch_data, batch_rep=batch_rep, iter_rep=iter_rep)
 
         return routes, costs
