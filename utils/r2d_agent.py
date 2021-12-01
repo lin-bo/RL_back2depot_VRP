Bug fix: numerical issue

utils/r2d_agent.py
@@ -81,6 +81,7 @@ class returnAgent:
         q = torch.cat((q_n, q_r), 1)
         # max value
         qind = torch.argmax(q, dim=1).reshape(batch, 1)
+        print(qind)
         if explore:
             exp_flag = (torch.rand((batch, 1), device=self.device) <= self.epsilon).to(torch.int32)
             qind = exp_flag * (1 - qind) + (1 - exp_flag) * qind
@@ -93,7 +94,7 @@ class returnAgent:
                 qind[i, 0] = 1
         # capacity
         for i, g in enumerate(dgl.unbatch(state.g)):
-            if torch.all(torch.logical_or(state.c < g.ndata["x"][1:,1], g.ndata["x"][1:,0])).item():
+            if torch.all(torch.logical_or((state.c + 1e-4) < g.ndata["x"][1:,1], g.ndata["x"][1:,0])).item():
                 qind[i, 0] = 1
         # get value
         action = (qind - 0.5) * 2

utils/state.py
@@ -126,7 +126,7 @@ class returnState:
             mask = torch.minimum(mask + flag_demand.reshape((self._batch, 1, -1)), torch.tensor(1, device=self.device))
         else:
             demand = torch.cat((torch.zeros(self._batch,1, device=self.device), demand), axis=1)
-            mask = torch.logical_or(self.o.to(torch.bool), (demand > self.c)).reshape(self._batch, 1, -1)
+            mask = torch.logical_or(self.o.to(torch.bool), (demand > (self.c + 1e-4))).reshape(self._batch, 1, -1)
         #    depot_mask = torch.cat([torch.ones((self._batch, 1), dtype=torch.float32, device=self.device),
         #                            torch.zeros((self._batch, self._size), dtype=torch.float32, device=self.device)],
         #                           axis=1).reshape((self._batch, 1, -1))